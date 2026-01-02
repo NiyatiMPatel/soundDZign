17b502598434db5ed9ca8369d5e3d877199ea799
NiyatiMPatel <patelnitie@gmail.com>
2026-01-02 20:53:26 +0530

    updated statistics section

--- src/sections/Statistics.jsx
+++ src/sections/Statistics.jsx
@@ -1,15 +1,21 @@
-import React from "react";
-
+import { statistics } from "../constants/constants";
 const Statistics = () => {
   return (
     <section id="statistics">
       <div className="container">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-        tempore animi, illo blanditiis tempora iste ipsam explicabo autem beatae
-        obcaecati repellendus reiciendis ullam eum modi aliquid veritatis rerum.
-        Soluta distinctio deleniti rerum facere amet, error, quia totam quod
-        numquam est eos a ratione consequuntur! Sit soluta nostrum distinctio
-        cumque autem.
+        <div className="row justify-content-around align-items-center stats-content position-relative">
+          {/* eslint-disable-next-line no-unused-vars */}
+          {statistics.map(({ id, Icon, stats_no, stats_title }) => (
+            <div
+              className="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center"
+              key={id}
+            >
+              <Icon width={100} height={100} />
+              <h2 className="stats_count">{stats_no}</h2>
+              <p className="stats_title">{stats_title}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
