fec8cb34246ef229006e1a414b7cabffb659eb60
NiyatiMPatel <patelnitie@gmail.com>
2026-01-05 16:34:55 +0530

    completed latest posts section

--- src/sections/Posts.jsx
+++ src/sections/Posts.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import { posts } from "../constants/constants";
 
 const Posts = () => {
   return (
     <section id="posts">
       <div className="container">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-        tempore animi, illo blanditiis tempora iste ipsam explicabo autem beatae
-        obcaecati repellendus reiciendis ullam eum modi aliquid veritatis rerum.
-        Soluta distinctio deleniti rerum facere amet, error, quia totam quod
-        numquam est eos a ratione consequuntur! Sit soluta nostrum distinctio
-        cumque autem.
+        <h2 className="eyeshadow eyeshadow-yellow position-relative section-title">
+          Latest Posts
+        </h2>
+        <div className="row posts-wrapper">
+          {posts.map(({ id, src, title, tag }) => (
+            <div className="col-12 col-md-6 col-lg-4 post-column" key={id}>
+              <div className="card h-100 position-relative">
+                <img
+                  src={src}
+                  loading="lazy"
+                  className="card-img-top"
+                  alt={title}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{title}</h5>
+                </div>
+                <div className="card-tag">{tag}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="btn-wrapper text-end">
+          <button>All Posts</button>
+        </div>
       </div>
     </section>
   );
